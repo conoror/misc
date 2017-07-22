b7c676d53365a1822d0fd7118598b6b026742ac7
conoror <falling174fps@gmail.com>
2017-07-22 17:22:15 +0100

    Windows Update pullpatches.bat July 2017 edition
    IE11 is a cumulative update and supersedes all previous

--- wupatch/pullpatches.bat
+++ wupatch/pullpatches.bat
@@ -5,7 +5,7 @@ rem Two echo offs just in case UTF-8 with byte order marker (github...)
 
 setlocal enableextensions enabledelayedexpansion
 
-set ppversion=13
+set ppversion=14
 set ppurl=https://raw.githubusercontent.com/conoror/misc/master/wupatch/pullpatches.bat
 
 set msdownloadpath=http://download.windowsupdate.com
@@ -298,18 +298,19 @@ Security only Updates for Oct,Nov,Dec 2016
 /c/msdownload/update/software/secu/2016/12/windows6.1-kb3205394-x64_71d0c657d24bc852f074996c32987fb936c07774.msu
 
 
-Security only Updates for Jan,Mar,Apr,May 2017
-----------------------------------------------
+Security only Updates for Jan-Jun 2017
+--------------------------------------
 
 /c/msdownload/update/software/secu/2017/01/windows6.1-kb3212642-x64_f3633176091129fc428d899c93545bdc7821e689.msu
 /d/msdownload/update/software/secu/2017/02/windows6.1-kb4012212-x64_2decefaa02e2058dcd965702509a992d8c4e92b3.msu
 /c/msdownload/update/software/secu/2017/03/windows6.1-kb4015546-x64_4ff5653990d74c465d48adfba21aca6453be99aa.msu
 /c/msdownload/update/software/secu/2017/05/windows6.1-kb4019263-x64_d64d8b6f91434754fdd2a552d8732c95a6e64f30.msu
+/c/msdownload/update/software/secu/2017/06/windows6.1-kb4022722-x64_ee5b5fae02d1c48dbd94beaff4d3ee4fe3cd2ac2.msu
 
 
-Security only and IE11 (IE is cumulative) Updates for June 2017
+Security only and IE11 (IE is cumulative) Updates for July 2017
 ---------------------------------------------------------------
 
-/c/msdownload/update/software/secu/2017/05/ie11-windows6.1-kb4021558-x64_ac613bb21b5ba702d60367b5db8c35ada8e65c50.msu
-/c/msdownload/update/software/secu/2017/06/windows6.1-kb4022722-x64_ee5b5fae02d1c48dbd94beaff4d3ee4fe3cd2ac2.msu
+/c/msdownload/update/software/secu/2017/07/ie11-windows6.1-kb4025252-x64_80edf86dd6b82f99214b6de9230c481b70995bd1.msu
+/c/msdownload/update/software/secu/2017/07/windows6.1-kb4025337-x64_c013b7fcf3486a0f71c4f58fc361bfdb715c4e94.msu
 
